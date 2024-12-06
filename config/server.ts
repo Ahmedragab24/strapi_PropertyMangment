fcbc90f08637d4c283fd75f11f2aa1ec3ab3f2e0
Ahmed Ragab <89437174+Ahmedragab24@users.noreply.github.com>
2024-12-07 01:10:51 +0200

    change varibale

--- config/server.ts
+++ config/server.ts
@@ -10,7 +10,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'), 
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://your-app-name.up.railway.app'),
+  url: env('PUBLIC_URL', 'https://strapipropertymangment-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
